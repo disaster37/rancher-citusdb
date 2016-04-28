d81aeba3c0ac67f9b96c84b1ebffc2a80c947ed0
Sebastien Langoureaux <sebastien.langoureaux@webcenter.fr>
2016-04-28 13:46:48 +0200

    fix dockerfile and init script

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,6 @@ ENV CITUS_WORKER_CONF_PATH "/etc/citus"
 COPY assets/init.py /app/
 
 
-VOLUME ["CITUS_WORKER_CONF_PATH"]
+VOLUME ["${CITUS_WORKER_CONF_PATH}"]
+
+CMD python /app/init.py
--- assets/init.py
+++ assets/init.py
@@ -13,17 +13,29 @@ class ServiceRun():
         # Get metadata to discovery worker
         metadata_manager = MetadataAPI()
         metadata_manager.wait_service_containers()
-
-        # Workers must be link to me with the name worker
-        linking_services = metadata_manager.get_service_links()
-
+        services = metadata_manager.get_stack_services()
+        service =  metadata_manager.get_service()
+        my_name = service['name']
         service_name = None
+        stack_name = None
 
-        for linking_service in linking_services:
-            search = re.search('([^/]+)/([^:])+:worker$', linking_service)
-            if search:
-                service_name = search.group(2)
-                stack_name = search.group(1)
+
+        # We search container that is in sidekick with me
+        for service in services:
+            for sidekick in service['sidekicks']:
+                if sidekick == my_name:
+                        # We search the worker service that must be linked with the master as name worker
+                        for linking_service in service['links']:
+                                if  service['links'][linking_service] == 'worker':
+                                        search = re.search('^([^/]+)/([^/]+)$', linking_service)
+                                        if search:
+                                                service_name = search.group(2)
+                                                stack_name = search.group(1)
+                                                break
+
+                        break
+
+
 
         if service_name is None:
             print("Standalone mode")
@@ -35,9 +47,14 @@ class ServiceRun():
 
         f = open(os.getenv('CITUS_WORKER_CONF_PATH') + '/pg_worker_list.conf', 'w')
 
-        for container in containers:
+
+        for name, container in containers.iteritems():
             f.write("%s 5432\n" % container['primary_ip'])
             print("Add worker %s (%s) on Citus cluster" % (container['primary_ip'], container['name']))
 
         f.close()
 
+if __name__ == '__main__':
+
+    serviceRun = ServiceRun()
+    serviceRun.run()
